063465531a46f6ff7b09fe7b60c19dc6b25307f6
fatihkayas <seavsdeep@gmail.com>
2025-06-12 14:36:58 +0200

    main.tf and variables.tf

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "rg-registry" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+resource "azurerm_container_registry" "rcteamdev" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg-registry.name
+  location            = azurerm_resource_group.rg-registry.location
+  sku                 = var.acr_sku
+  admin_enabled       = var.acr_admin_enabled
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,50 @@
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "team1-rg-manual"
+}
+
+variable "resource_group_location" {
+  description = "Location for the resource group"
+  type        = string
+  default     = "northeurope"
+}
+
+variable "acr_name" {
+  description = "Name of the Azure Container Registry"
+  type        = string
+  default     = "team1acrmanual"
+}
+
+variable "acr_sku" {
+  description = "SKU for the Azure Container Registry"
+  type        = string
+  default     = "Standard"
+}
+
+variable "acr_admin_enabled" {
+  description = "Enable admin user for the ACR"
+  type        = bool
+  default     = true
+}
