9b18b4f8e9fee4f57d0ae2d8165b6fd8bcb50d73
fatihkayas <seavsdeep@gmail.com>
2025-07-01 17:17:35 +0200

    fix dev.tfvars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -22,7 +22,7 @@ variable "tenant_id" {
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
-  default     = "team1-rg-manual2"
+  default     = "team1-rg-manual3"
 }
 
 variable "resource_group_location" {
@@ -34,7 +34,7 @@ variable "resource_group_location" {
 variable "acr_name" {
   description = "Name of the Azure Container Registry"
   type        = string
-  default     = "team1acrmanual2"
+  default     = "team1acrmanual3"
 }
 
 variable "acr_sku" {
