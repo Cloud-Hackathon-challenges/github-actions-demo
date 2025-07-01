daa82cb32dfba878c6159dea652d916b77c13f97
fatihkayas <seavsdeep@gmail.com>
2025-07-01 16:46:16 +0200

    fix dev.tfvars

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg-registry" {
   name     = var.resource_group_name
   location = var.resource_group_location
-}
+} 
 
 resource "azurerm_service_plan" "asp" {
   name                = "asp-${var.resource_group_name}"
