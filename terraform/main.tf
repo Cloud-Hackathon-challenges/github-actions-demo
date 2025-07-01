a8d30f8daa97ed209e901ffc29d431b8a2594f05
fatihkayas <seavsdeep@gmail.com>
2025-07-01 17:21:09 +0200

    fix dev.tfvars

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,7 +37,7 @@ resource "azurerm_container_registry" "rcteamdev" {
 }
 
 resource "azurerm_linux_web_app" "as" {
-  name                = "as"
+  name                = "ass"
   resource_group_name = azurerm_resource_group.rg-registry.name
   location            = azurerm_resource_group.rg-registry.location
   service_plan_id     = azurerm_service_plan.asp.id
