eb3482ca3cc569f3e3e4754f8cf048dd88de51aa
fatihkayas <seavsdeep@gmail.com>
2025-07-03 09:38:06 +0200

    fix context

--- test/cypress.config.js
+++ test/cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://ass23847.azurewebsites.net",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
