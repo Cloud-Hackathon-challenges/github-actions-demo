7e51b38815bac1f3cd0fb48aece6cb57ddf3e59e
fatihkayas <seavsdeep@gmail.com>
2025-07-03 13:40:49 +0200

    add path test

--- test/cypress.config.js
+++ test/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://ass23847.azurewebsites.net",
+    baseUrl: "http://localhost/books",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
