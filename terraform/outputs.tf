6fbb8e6f2be0afde5bc397f4f7fe64c374191394
fatihkayas <seavsdeep@gmail.com>
2025-08-14 13:25:02 +0200

    change name of acr to 9

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,16 +1,11 @@
-output "acr_name" {
-  value = azurerm_container_registry.rcteamdev.name
+output "resource_group_name" {
+  value = azurerm_resource_group.rg-registry.name
 }
 
-output "acr_login_server" {
-  value = azurerm_container_registry.rcteamdev.login_server
+output "app_service_plan" {
+  value = azurerm_service_plan.asp.name
 }
 
-output "acr_admin_username" {
-  value = azurerm_container_registry.rcteamdev.admin_username
-}
-
-output "acr_admin_password" {
-  value     = azurerm_container_registry.rcteamdev.admin_password
-  sensitive = true
+output "app_service_name" {
+  value = azurerm_linux_web_app.as.name
 }
