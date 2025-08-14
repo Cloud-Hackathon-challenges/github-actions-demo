ed50e5bbc4446841a218f5992c9bd9f9b5585428
fatihkayas <seavsdeep@gmail.com>
2025-08-14 13:47:58 +0200

    fix outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,20 @@ output "app_service_plan" {
 output "app_service_name" {
   value = azurerm_linux_web_app.as.name
 }
+
+output "acr_name" {
+  value = azurerm_container_registry.rcteamdev.name
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.rcteamdev.login_server
+}
+
+output "acr_admin_username" {
+  value = azurerm_container_registry.rcteamdev.admin_username
+}
+
+output "acr_admin_password" {
+  value     = azurerm_container_registry.rcteamdev.admin_password
+  sensitive = true
+}
