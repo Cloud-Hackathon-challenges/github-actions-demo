3f0e20693c3e54548dde9ecc7dd5c22640a11185
fatihkayas <seavsdeep@gmail.com>
2025-10-08 16:45:39 +0200

    fix: cypress issues

--- test/cypress/e2e/cypress.config.js
+++ test/cypress/e2e/cypress.config.js
@@ -1,7 +1,7 @@
-const { defineConfig } = require('cypress');
+// const { defineConfig } = require('cypress');
 
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: process.env.CYPRESS_BASE_URL || 'https://ass238471.azurewebsites.net',
-  },
-});
+// module.exports = defineConfig({
+//   e2e: {
+//     baseUrl: process.env.CYPRESS_BASE_URL || 'https://ass238471.azurewebsites.net',
+//   },
+// });
--- test/cypress/e2e/spec2.cy.js
+++ test/cypress/e2e/spec2.cy.js
@@ -1,12 +1,5 @@
-// describe('template spec', () => {
-//   it('passes', () => {
-//     cy.visit('https://ass238471.azurewebsites.net/admin')  // same here
-//   })
-// })
-
 describe('template spec', () => {
-  it('opens admin', () => {
-    cy.visit('/admin'); // hardcoded URL yok
-  });
-});
-
+  it('passes', () => {
+    cy.visit('https://ass238471.azurewebsites.net/admin')  // same here
+  })
+})
